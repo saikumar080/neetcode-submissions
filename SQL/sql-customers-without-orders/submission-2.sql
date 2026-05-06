f09129ca162a584cc72b5851dad193fab8be389e
saikumar pillalamarri <150997622+saikumar080@users.noreply.github.com>
2026-05-07 04:57:08 +0530

    Add: sql-customers-without-orders - submission-2

---
+++ SQL/sql-customers-without-orders/submission-2.sql
@@ -0,0 +1,13 @@
+-- Write your query below
+-- SELECT c.name
+-- FROM customers c
+-- LEFT JOIN orders o
+-- on c.id =o.customer_id
+-- WHERE o.customer_id IS NULL;
+SELECT name
+FROM customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE c.id = o.customer_id
+);
